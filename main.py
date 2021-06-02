1e35e96ebe5dc5084bae16f9658f697800f70d4b
supremeicecreme <thebullasjr@gmail.com>
2021-06-02 12:29:24 +0100

    Removed some testing fluff

--- main.py
+++ main.py
@@ -2,7 +2,4 @@ import discordHandler
 import dataAccess
 
 if __name__ == "__main__":
-    guild_id = "395274402901524480"
-    tags = dataAccess.get_tags_by_guild(guild_id)
-    print(tags)
     discordHandler.kickstart()
